29fa763830ec22bf33f801f9b67e3baae0bf5b25
Erika Sanchez <erika.walker.business@gmail.com>
2021-02-24 22:25:34 -0500

    Pulled data?

--- script.js
+++ script.js
@@ -22,18 +22,57 @@ const panels = document.querySelectorAll('.panel');
 
 console.log(panels)
 
-tabs.addEventListener('click', function(e) {
+tabs.addEventListener('click', function (e) {
     if (e.target.tagName == "LI") {
         const targetPanel = document.querySelector(e.target.dataset.target);
-        panels.forEach(function(panel) {
+        panels.forEach(function (panel) {
             if (panel == targetPanel) {
                 panel.classList.add('active');
             } else {
                 panel.classList.remove('active');
             }
-        }) 
+        })
     }
 })
+
+// '9mdi1ioUnMia5bH3RgEK'
+
+const app = document.getElementById('movies');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
+let request = new XMLHttpRequest();
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.onload = function () {
+
+    let data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(movie => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+            
+            const p = document.createElement("p");
+            movie.description = movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...`;
+
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);
+        });
+    } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
+    }
+}
+
+request.send();
 
 
 
