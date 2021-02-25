6eaf28fd3853ad888e88c85620efa537b226cfe9
Erika Sanchez <erika.walker.business@gmail.com>
2021-02-24 19:39:53 -0500

    Try at making tabs. They work.

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-function openInfo(evt, tabName) {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+console.log("Javascript is loaded!")
 
-    tablinks = document.getElementsByClassName("tablinks"); 
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+// function openInfo(evt, tabName) {
+//     let i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
 
-    document.getElementById(tabName)
-    tabName.style.display = "block";
-    evt.currentTarget.className += " active";
-}
+//     tablinks = document.getElementsByClassName("tablinks"); 
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+
+//     document.getElementById(tabName)
+//     tabName.style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
+
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+
+console.log(panels)
+
+tabs.addEventListener('click', function(e) {
+    if (e.target.tagName == "LI") {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel) {
+            if (panel == targetPanel) {
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        }) 
+    }
+})
+
+
 
